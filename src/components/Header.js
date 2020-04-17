f6eb57985ecdf443ff433726ece0196ded597ae6
AliakseiLasevich <alexey.losevich@gmail.com>
2020-04-18 01:29:58 +0300

    12

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+
+    return <div className="header">
+        <div className="logo">
+            <img src="https://www.ggau.by/images/ggau-logo-m.png" alt="{}"/>
+        </div>
+    </div>
+}
+
+export default Header;
---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Menu = ()=>{
+    return <div className="menu">
+        <div className="menu-item">Факультеты</div>
+        <div className="menu-item">Кафедры</div>
+        <div className="menu-item">Преподаватели</div>
+        <div className="menu-item">Корпуса</div>
+        <div className="menu-item">Кабинеты</div>
+    </div>
+}
+
+export default Menu;
