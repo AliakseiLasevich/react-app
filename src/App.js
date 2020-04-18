627f7776b574aea0541b94589f87abf5d54467df
AliakseiLasevich <alexey.losevich@gmail.com>
2020-04-18 21:48:47 +0300

    19

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import Header from "./components/Header/Header";
 import Menu from "./components/Menu/Menu";
 import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
     return (
-        <div className="wrapper">
-            <Header/>
-            <Menu/>
-            <Content/>
-            <Footer/>
-        </div>
+        <BrowserRouter>
+            <div className="wrapper">
+                <Header/>
+                <Menu/>
+                <Content/>
+                <Footer/>
+            </div>
+        </BrowserRouter>
     );
 }
 
 export default App;
+
--- src/components/ControlPanel/ControlPanel.jsx
+++ src/components/ControlPanel/ControlPanel.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import style from "./ControlPanel.module.css";
 import AddFaculty from "./AddFaculty/AddFaculty";
 import AddCathedra from "./AddCathedra/AddCathedra";
+import {Route} from "react-router-dom"
 
 const ControlPanel = (props) => {
     return <div className={style.controlPanel}>
-        <AddFaculty />
-        <AddCathedra/>
+        <ul>
+            <li><a href="/addFaculty">Добавить факультет</a></li>
+            <li><a href="/addCathedra">Добавить кафедру</a></li>
+        </ul>
+        <Route path="/addFaculty" component={AddFaculty}/>
+        <Route path="/addCathedra" component={AddCathedra}/>
 
     </div>
 }
