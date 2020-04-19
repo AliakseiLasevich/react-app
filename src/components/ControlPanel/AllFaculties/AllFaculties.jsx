2f984a166fb6ef5505c9a23ae73e23384d5fd535
AliakseiLasevich <alexey.losevich@gmail.com>
2020-04-19 20:22:27 +0300

    27

--- src/components/ControlPanel/AllFaculties/AllFaculties.jsx
+++ src/components/ControlPanel/AllFaculties/AllFaculties.jsx
@@ -3,18 +3,7 @@ import style from "./AllFaculties.module.css";
 import Faculty from "../Faculty/Faculty";
 
 const AllFaculties = (props) => {
-
-    let allFaculties = [
-        {name: "Агрономический"},
-        {name: "Защиты растений"},
-        {name: "Ветеринарной медицины"},
-        {name: "Биотехнологический"},
-        {name: "Инженерно-технологический"},
-        {name: "Экономический факультет"},
-        {name: "Бухгалтерского учёта"},
-    ]
-
-    let facultiesWithName = allFaculties.map(faculty => <Faculty name={faculty.name}/>);
+    let facultiesWithName =props.allFaculties.map(faculty => <Faculty name={faculty.name}/>);
 
     return (
         <div className={style.AllFaculties}>
--- src/components/ControlPanel/FacultyManager/FacultyManager.jsx
+++ src/components/ControlPanel/FacultyManager/FacultyManager.jsx
@@ -5,17 +5,30 @@ import AddFaculty from "../AddFaculty/AddFaculty";
 import AllFaculties from "../AllFaculties/AllFaculties";
 
 const FacultyManager = (props) => {
+
+    let allFaculties = [
+        {name: "Агрономический"},
+        {name: "Защиты растений"},
+        {name: "Ветеринарной медицины"},
+        {name: "Биотехнологический"},
+        {name: "Инженерно-технологический"},
+        {name: "Экономический факультет"},
+        {name: "Бухгалтерского учёта"},
+    ];
+
     return (
         <div className={style.FacultyManager}>
 
             Управление факультетами.
-            <li><NavLink activeClassName={style.active} to="/facultyManager/addFaculty">Добавить факультет</NavLink></li>
-            <li><NavLink activeClassName={style.active} to="/facultyManager/allFaculties">Просмотреть все факультеты</NavLink></li>
+            <li><NavLink activeClassName={style.active} to="/facultyManager/addFaculty">Добавить факультет</NavLink>
+            </li>
+            <li><NavLink activeClassName={style.active} to="/facultyManager/allFaculties">Просмотреть все
+                факультеты</NavLink></li>
 
             {/*TODO  Добавить подменю. Разобраться с вложенным роутингом*/}
 
             <Route path="/facultyManager/addFaculty" component={AddFaculty}/>
-            <Route path="/facultyManager/allFaculties" component={AllFaculties}/>
+            <Route path="/facultyManager/allFaculties" render={() => <AllFaculties allFaculties={allFaculties}/>}/>
 
         </div>
     )
