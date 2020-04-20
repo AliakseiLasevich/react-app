e5da616dea9f2a474677758fde563025a16169a0
AliakseiLasevich <alexey.losevich@gmail.com>
2020-04-20 23:19:31 +0300

    34 flux

--- src/components/ControlPanel/AddFaculty/AddFaculty.jsx
+++ src/components/ControlPanel/AddFaculty/AddFaculty.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import style from "./AddFaculty.module.css";
-import {redrawTextInput} from "../../../state/state";
 
 const AddFaculty = (props) => {
-
-
 
     let newFacultyElement = React.createRef();
 
@@ -12,7 +9,6 @@ const AddFaculty = (props) => {
         let text = newFacultyElement.current.value;
         props.addFaculty(text);
         props.redrawTextInput("");
-        debugger
     };
 
     let onInputChange = ()=>{
@@ -22,7 +18,9 @@ const AddFaculty = (props) => {
 
     return (
         <div className={style.addFaculty}>
-            <input type="text" ref={newFacultyElement} onChange={onInputChange} value={props.state.inputTextField} />
+            <input type="text" ref={newFacultyElement}
+                   onChange={onInputChange}
+                   value={props.state.inputTextField} />
             <div><input type="button" value="+факультет" onClick={addFaculty}/></div>
         </div>
 
