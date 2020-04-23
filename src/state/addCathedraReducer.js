2c22c86e2b736a169ae70c98acd1354199fc458e
AliakseiLasevich <alexey.losevich@gmail.com>
2020-04-23 14:41:53 +0300

    41 reducer. if-else to switch-case refactor

--- src/state/addCathedraReducer.js
+++ src/state/addCathedraReducer.js
@@ -2,15 +2,18 @@ const ADD_CATHEDRA = "ADD-CATHEDRA";
 const CATHEDRA_UPDATE_TEXT_FIELD = "CATHEDRA-UPDATE-TEXT-FIELD";
 
 const addCathedraReducer = (state, action) => {
-
-    if (action.type === CATHEDRA_UPDATE_TEXT_FIELD) {
-        state.cathedraInputTextField = action.inputText;
-    } else if (action.type === ADD_CATHEDRA) {
-        let cathedra = {name: action.cathedraName};
-        state.cathedraInputTextField = "";
-        state.allCathedras.push(cathedra);
+    switch (action.type) {
+        case ADD_CATHEDRA:
+            let cathedra = {name: action.cathedraName};
+            state.cathedraInputTextField = "";
+            state.allCathedras.push(cathedra);
+            return state;
+        case CATHEDRA_UPDATE_TEXT_FIELD:
+            state.cathedraInputTextField = action.inputText;
+            return state;
+        default:
+            return state;
     }
-    return state;
-}
+};
 
 export default addCathedraReducer;
--- src/state/addFacultyReducer.js
+++ src/state/addFacultyReducer.js
@@ -4,15 +4,20 @@ const ADD_FACULTY = "ADD-FACULTY";
 const FACULTY_UPDATE_TEXT_FIELD = "FACULTY-UPDATE-TEXT-FIELD";
 
 export const addFacultyReducer = (state, action) => {
-    if (action.type === ADD_FACULTY) {
-        let faculty = {name: action.facultyName};
-        state.facultyInputTextField = "";
-        state.allFaculties.push(faculty);
-    } else if (action.type === FACULTY_UPDATE_TEXT_FIELD) {
-        state.facultyInputTextField = action.inputText;
-    }
+    switch (action.type) {
 
-    return state;
+        case  ADD_FACULTY:
+            let faculty = {name: action.facultyName};
+            state.facultyInputTextField = "";
+            state.allFaculties.push(faculty);
+            return state;
+
+        case FACULTY_UPDATE_TEXT_FIELD:
+            state.facultyInputTextField = action.inputText;
+            return state;
+        default:
+            return state;
+    }
 }
 
 export default addFacultyReducer;
